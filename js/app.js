4b8c5ac931c161ac71630eb11ed0de40afb9506d
abdullox0900 <abdullox0900@gmail.com>
2022-02-07 14:16:37 +0500

    add app js api

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 let elBtn = document.querySelector(".btn");
 let elImg = document.querySelector(".cat-img");
+
+const apiUrl = "https://random.imagecdn.app/600/400";
+
+
+//OPTION 1
+const getImgApi = async function () {
+     const response = await fetch(apiUrl);
+
+     elImg.src = response.url;
+
+     console.log(response);
+}
+
+getImgApi()
+
+elBtn.addEventListener("click", function () {
+     getImgApi()
+})
